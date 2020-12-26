783ac1084b44fa1987666561297f69639838b219
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2020-12-26 16:59:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass('sticky');
+        }else{
+            $(".navbar").removeClass('sticky');
+        }
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // ow carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
