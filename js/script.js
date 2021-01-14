1ce290586b017f11b8f6f04ba75ad73f5fb0fb82
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-01-15 00:59:26 +0600

    type content change

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
     //typing animation script
     var typed = new Typed(".typing", {
-        strings: ["Android Developer","YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Android Developer","Flutter Developer", "Backend Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
